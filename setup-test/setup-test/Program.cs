c19ccbf3b5962ef752a32c34b77af62457c002d3
Mitchell-kolb <mkolb321@gmail.com>
2025-05-11 23:58:38 -0700

    Asp test

--- setup-test/setup-test/Program.cs
+++ setup-test/setup-test/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! This is different now");
+﻿// File: Program.cs
+using System.Net.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Define a single GET endpoint at the root path
+app.MapGet("/", async (HttpContext httpContext) =>
+{
+    var message = "Hello, world!";
+    await httpContext.Response.WriteAsync(message);
+});
+
+app.Run();
