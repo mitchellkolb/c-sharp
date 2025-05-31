e2a549bd75493a978b544ea22f5edf192f6cf9b3
Mitchell-kolb <mkolb321@gmail.com>
2025-05-30 23:52:33 -0700

    Compelted 2 Numeric Types Walkthrough

--- windows/full-course/2-numeric-data-types/Program.cs
+++ windows/full-course/2-numeric-data-types/Program.cs
@@ -13,7 +13,30 @@ namespace Datatypes
 
             Console.WriteLine(age);
             Console.WriteLine(int.MaxValue);
-            Console.WriteLine
-        }
+            Console.WriteLine(int.MinValue);
+
+            long bigNumber = -9000000000L;
+            Console.WriteLine(bigNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+            double negative = -55.2D;
+            Console.WriteLine(negative);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            float precision = 5.000001F;
+            Console.WriteLine(precision);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(float.MinValue);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+
+
+            }
     }
 }
